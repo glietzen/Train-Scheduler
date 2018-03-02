cba6c56bf593ee398bd411e61a9c50f0b50d16a5
Garrett Lietzen <glietzen@gmail.com>
2018-03-01 19:59:04 -0600

    fully functioning now

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     //Initialize Firebase
     var config = {
         apiKey: "AIzaSyC_RsQ2frEGYUoprVaVaG88e-uiBor9fVU",
@@ -8,25 +8,74 @@ $(document).ready(function () {
         projectId: "train-schedule-5c149",
         storageBucket: "train-schedule-5c149.appspot.com",
         messagingSenderId: "682965701211"
-      };
+    };
 
-      firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-      var database = firebase.database();
+    var database = firebase.database();
 
-      var newRow = '';
-      var trainName = '';
-      var destination = '';
-      var firstDeparture = '';
-      var frequency = '';
-      var nextArrival = '';
-      var minsAway = '';
-      var now = moment();
+    var newRow = '';
+    var trainName = '';
+    var destination = '';
+    var firstDeparture = '';
+    var frequency = '';
+    var nextArrival = '';
+    var minsAway = '';
+    var now = moment();
 
-      $('#submitBtn').on('click', function() {
-          event.preventDefault();
-          trainName = $()
-      })
+    $('#submitBtn').on('click', function () {
+        event.preventDefault();
+        trainName = $('#nameInput').val().trim();
+        destination = $('#destinationInput').val().trim();
+        firstDeparture = $('#firstDepartureInput').val().trim();
+        frequency = $('#frequencyInput').val().trim();
+        //   console.log('Train Name: ' + trainName + ' / Destination: ' + destination + ' / First Departure: ' + firstDeparture + ' / Frequency: ' + frequency);
+
+
+        database.ref('/train-schedule').push({
+            trainName: trainName,
+            destination: destination,
+            firstDeparture: firstDeparture,
+            frequency: frequency
+        });
+
+    });
+
+    database.ref('/train-schedule').on('child_added', function (snapshot) {
+        var sv = snapshot.val();
+
+        var initialTimeConverted = moment(sv.firstDeparture, "hh:mm").subtract(1, "years");
+        console.log(initialTimeConverted);
+
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        var diffTime = moment().diff(moment(initialTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        var timeRemainder = diffTime % sv.frequency;
+        console.log("TIME REMAINDER = " + timeRemainder);
+
+        minsAway = sv.frequency - timeRemainder;
+        console.log("MINUTES UNTIL NEXT TRAIN: " + minsAway);
+
+        arrival = moment().add(minsAway, "minutes");
+        // arrival = moment(arrival).format("hh:mm");
+        console.log("ARRIVAL TIME: " + moment(arrival).format("hh:mm"));
+
+        var row = $('<tr>');
+        row.append($('<td>' + sv.trainName + '</td>'));
+        row.append($('<td>' + sv.destination + '</td>'));
+        row.append($('<td>' + sv.firstDeparture + '</td>'));
+        row.append($('<td>' + sv.frequency + '</td>'));
+        row.append($('<td>' + moment(arrival).format('hh:mm') + '</td>'));
+        row.append($('<td>' + minsAway + '</td>'));
+
+        console.log(row);
+
+        $('.table').append(row);
+
+    })
 
 
 
